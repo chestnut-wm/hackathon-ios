04a98e087e6963396097fa08a9eb2d3af560800d
Calvin Chestnut <cchestnut@weedmaps.com>
2024-08-20 11:47:03 -0700

    Initial Commit

---
+++ CardReadingComponentsTests/CardReadingComponentsTests.swift
@@ -0,0 +1,16 @@
+//
+//  CardReadingComponentsTests.swift
+//  CardReadingComponentsTests
+//
+//  Created by Calvin Chestnut on 8/20/24.
+//
+
+import Testing
+
+struct CardReadingComponentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
